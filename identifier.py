b8aac5c6003e4bdd0e5735b855c184fbe2584243
Anshu Sharma <112000@augroup.com>
2026-04-03 17:44:34 +0530

    Workflow

--- identifier.py
+++
@@ -1,6 +0,0 @@
-def identify(file_path):
-    if file_path.lower().endswith(".pdf"):
-        return "PDF"
-    elif file_path.lower().endswith((".jpg", ".jpeg", ".png")):
-        return "IMAGE"
-    return "UNKNOWN"
